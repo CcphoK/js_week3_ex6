69b245a2e9259ec1e1f4ba65b2a47999e0b05eee
CcphoK <flavoirkeswa@gmail.com>
2025-11-11 15:53:02 +0200

    week 3 exercise 6

---
+++ ex_6_functions.js
@@ -0,0 +1,20 @@
+// TODO: Create a function called greet that takes a name parameter 
+// TODO: Make the greet function return "Hello, [name]!" 
+// TODO: Create a function called add that takes two parameters 
+// TODO: Make the add function return the sum of both parameters 
+// TODO: Call both functions and store their results in variables 
+// TODO: Print the results to the console
+
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+let greeting = greet("Sisipho");
+let sumResult = add(5, 7);
+
+console.log(greeting);
+console.log("The sum is :" + sumResult);
